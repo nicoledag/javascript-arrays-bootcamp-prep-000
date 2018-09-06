542d2308a3082a74ee07d51a9898cd7908cfd89a
Nicole D <nnardella@gmail.com>
2018-09-06 15:20:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -52,7 +52,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 
 
 function removeElementFromEndOfArray(array){
- array.slice(0, array.length - 1);
+ array.slice(1, array.length - 1);
  return array
 
 }
